6e91a767e22f38efaf489a21f0718032c63ac4d0
adson <adsongabrielfsilva@gmail.com>
2025-03-23 11:12:14 -0300

    docs: adding documentation to Referencia.java

--- src/dica/Referencia.java
+++ src/dica/Referencia.java
@@ -2,12 +2,16 @@ package dica;
 
 import middleware.Validator;
 
-public class Referencia implements IAnexo{
+/**
+ * A classe {@code Referencia} representa um anexo do tipo referência bibliográfica que pode ser adicionado a uma dica.
+ * Ela implementa a interface {@code IAnexo} e fornece métodos para gerar resumos, detalhes e calcular o bônus associado ao anexo.
+ */
+public class Referencia implements IAnexo {
 	private String fonte, titulo;
 	private boolean verificado;
 	private int ano, relevancia;
 
-	public Referencia(String titulo, String fonte, boolean verificado, int ano, int relevancia){
+	public Referencia(String titulo, String fonte, boolean verificado, int ano, int relevancia) {
 		Validator.verifyStringBlank(titulo, "TITULO");
 		Validator.verifyStringBlank(fonte, "FONTE");
 
@@ -18,15 +22,18 @@ public class Referencia implements IAnexo{
 		this.relevancia = relevancia;
 	}
 
-	public String getResumo(){
+	@Override
+	public String getResumo() {
 		return this.titulo + "\n" + this.fonte + "\n" + this.ano;
 	}
 
-	public String getDetalhado(){
-		return this.titulo + "\n" + this.fonte + "\n" + this.relevancia + "/5\n" + "verificado: "+ (this.verificado ? "Sim\n":"Não\n") + this.ano;
+	@Override
+	public String getDetalhado() {
+		return this.titulo + "\n" + this.fonte + "\n" + this.relevancia + "/5\n" + "verificado: " + (this.verificado ? "Sim\n" : "Não\n") + this.ano;
 	}
 
-	public double calcularBonus(){
-		return (this.verificado? 15 : 0);
+	@Override
+	public double calcularBonus() {
+		return (this.verificado ? 15 : 0);
 	}
 }
