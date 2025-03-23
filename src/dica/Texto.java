fcd2c23ae5e48564e184d87284cce1d409f8ce6f
adson <adsongabrielfsilva@gmail.com>
2025-03-23 11:12:31 -0300

    docs: adding documentation to Texto.java

--- src/dica/Texto.java
+++ src/dica/Texto.java
@@ -2,28 +2,33 @@ package dica;
 
 import middleware.Validator;
 
-public class Texto implements IAnexo{
+/**
+ * A classe {@code Texto} representa um anexo do tipo texto que pode ser adicionado a uma dica.
+ * Ela implementa a interface {@code IAnexo} e fornece métodos para gerar resumos, detalhes e calcular o bônus associado ao anexo.
+ */
+public class Texto implements IAnexo {
 	private String texto;
 
-	public Texto(String texto){
+	public Texto(String texto) {
 		Validator.verifyStringBlank(texto, "TEXTO");
-
 		this.texto = texto;
 	}
 
-	public String getResumo(){
+	@Override
+	public String getResumo() {
 		return this.texto;
 	}
 
-	public String getDetalhado(){
+	@Override
+	public String getDetalhado() {
 		return this.texto + "\n" + this.texto.length();
 	}
 
-	public double calcularBonus(){
-		if(this.texto.length() < 100){
+	@Override
+	public double calcularBonus() {
+		if (this.texto.length() < 100) {
 			return 0;
 		}
-
 		return this.texto.length() / 10;
 	}
 }
