4ec9e7a4ded160eaff2d5740f60be65a3caf8589
adson <adsongabrielfsilva@gmail.com>
2025-03-23 11:11:02 -0300

    docs: adding documentation to IAnexo.java

--- src/dica/IAnexo.java
+++ src/dica/IAnexo.java
@@ -1,7 +1,16 @@
 package dica;
 
-interface IAnexo {
-	public String getResumo();
-	public String getDetalhado();
-	public double calcularBonus();
+/**
+ * A interface {@code IAnexo} define o contrato para anexos que podem ser adicionados a uma dica.
+ */
+public interface IAnexo {
+    public String getResumo();
+    public String getDetalhado();
+
+    /**
+     * Calcula o bônus associado ao anexo. O bônus é um valor numérico que representa a contribuição do anexo para a pontuação da dica.
+     *
+     * @return O valor do bônus calculado para o anexo.
+     */
+    public double calcularBonus();
 }
