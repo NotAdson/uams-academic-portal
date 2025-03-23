fb25c4f263ee2384a30f581cf20948395a2017b7
adson <adsongabrielfsilva@gmail.com>
2025-03-23 11:13:20 -0300

    docs: adding documentation to Validator

--- src/middleware/Validator.java
+++ src/middleware/Validator.java
@@ -1,23 +1,44 @@
 package middleware;
 
+/**
+ * A classe {@code Validator} fornece métodos utilitários para validação de strings.
+ * Ela contém métodos para verificar se uma string contém apenas dígitos e para garantir que uma string não seja nula ou vazia.
+ */
 public class Validator {
-	public static boolean isStringDigit(String str){
-		if(str == null || str.isBlank()){
+
+	/**
+	 * Verifica se uma string contém apenas caracteres numéricos (dígitos).
+	 *
+	 * @param str A string a ser verificada.
+	 * @return {@code true} se a string contiver apenas dígitos, se não, {@code false}, e se a String for nula ou vazia.
+	 */
+	public static boolean isStringDigit(String str) {
+		if (str == null || str.isBlank()) {
 			return false;
 		}
 
-		for(int i = 0; i < str.length(); i++){
-			if(!Character.isDigit(str.charAt(i))){
+		for (int i = 0; i < str.length(); i++) {
+			if (!Character.isDigit(str.charAt(i))) {
 				return false;
 			}
 		}
 		return true;
 	}
 
-	public static void verifyStringBlank(String str, String nomeArg){
-		if(str == null){
+	/**
+	 * Verifica se uma string é nula ou vazia (após remoção de espaços em branco).
+	 * Se a string for nula, uma exceção {@code NullPointerException} é lançada.
+	 * Se a string for vazia, uma exceção {@code IllegalArgumentException} é lançada.
+	 *
+	 * @param str      A string a ser verificada.
+	 * @param nomeArg  O nome do argumento que está sendo verificado (usado na mensagem de exceção).
+	 * @throws NullPointerException      Se a string for nula.
+	 * @throws IllegalArgumentException Se a string for vazia.
+	 */
+	public static void verifyStringBlank(String str, String nomeArg) {
+		if (str == null) {
 			throw new NullPointerException(nomeArg + " NULO!");
-		}else if(str.isBlank()){
+		} else if (str.isBlank()) {
 			throw new IllegalArgumentException(nomeArg + " VAZIO!");
 		}
 	}
