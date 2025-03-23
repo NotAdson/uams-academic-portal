ed5d569088d92e146511915c5ba594a6bca33c80
adson <adsongabrielfsilva@gmail.com>
2025-03-23 09:11:35 -0300

    feat: creating atividade extensao

---
+++ src/atividade/Extensao.java
@@ -0,0 +1,28 @@
+package atividade;
+
+import middleware.Validator;
+
+public class Extensao extends Atividade{
+	private String subtipo;
+
+	public Extensao(String codigo, int tempo, String subtipo){
+		super(codigo, "PESQUISA_EXTENSAO", 12 / 10, tempo);
+
+		Validator.verifyStringBlank(codigo, "CODIGO");
+		Validator.verifyStringBlank(subtipo, "SUBTIPO");
+		
+		if(tempo < 0){
+			throw new IllegalArgumentException("TEMPO MENOR QUE 0");
+		}
+
+		this.subtipo = subtipo;
+	}
+
+	public String getSubtipo(){
+		return this.subtipo;
+	}
+
+	public void setSubtipo(String subtipo){
+		this.subtipo = subtipo;
+	}
+}
