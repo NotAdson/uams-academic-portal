c2be6ce7be3993ddb58a281215513d915954e2c6
adson <adsongabrielfsilva@gmail.com>
2025-03-25 14:31:45 -0300

    feat: creating Usuario

---
+++ src/usuario/Usuario.java
@@ -0,0 +1,56 @@
+package usuario;
+
+public class Usuario implements Comparable<Usuario>{
+	
+	private String cpf, matricula, senha, nome;
+	private double quantidadeBonus;
+
+	public Usuario(String cpf, String senha, String nome, String matricula){
+		this.cpf = cpf;
+		this.senha = senha;
+		this.matricula = matricula;
+		this.nome = nome;
+	}
+
+	public String getNome(){
+		return this.nome;
+	}
+
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+
+	public String getCpf(){
+		return this.cpf;
+	}
+
+	public String getMatricula(){
+		return this.matricula;
+	}
+
+	public double getBonus(){
+		return this.quantidadeBonus;
+	}
+
+	public void setBonus(double bonus){
+		this.quantidadeBonus = bonus;
+	}
+
+	public boolean verificaSenha(String senha){
+		return this.senha.equals(senha);
+	}
+
+	public void setSenha(String senha){
+		this.senha = senha;
+	}
+
+	@Override
+	public int compareTo(Usuario other){
+		return this.nome.compareTo(other.nome);
+	}
+
+	@Override
+	public String toString(){
+		return this.nome + ", " + this.cpf + ", " + this.matricula;
+	}
+}
